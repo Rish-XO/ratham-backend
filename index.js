b06066264a7e448b7de0748f19908d8d747a5420
Rishal <Rishal.mhd009@gmail.com>
2023-08-20 12:59:07 +0530

    login workd

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.post("/student/login", async (req, res) => {
+  const { universityId, password } = req.body;
+  try {
+    //check if user exists
+    const user = await pool.query(
+      "SELECT * FROM students WHERE college_id = $1",
+      [universityId]
+    );
+    if (user.rows.length > 0) {
+      const token = user.rows[0].token;
+      res.json({ token });
+    } else {
+      // if user doesn't exist create new
+      const user = await pool.query(
+        "INSERT INTO students (college_id , password) VALUES ($1 , $2) RETURNING *",
+        [universityId, password]
+      );
+      const token = user.rows[0].token;
+      res.json({ token });
+    }
+  } catch (error) {
+    console.log("error from student login", error.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log("listening on 5000");
 });
